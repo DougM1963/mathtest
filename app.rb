4af136b2c43e26006c42e4bd223b9f7aae765490
dougm1963 <dougmeador1963@gmail.com>
2018-01-10 19:33:44 -0500

    finished

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require "sinatra"
+require_relative "mathprogram.rb"
+
 get '/' do
 	erb :home
 end
@@ -15,13 +17,13 @@ get '/math_input' do
 	p1 = params[:p1]
 	p2 = params[:p2]
 	if result == "addition"
-		erb :addition, :locals => {:result => result, :p1 => p1, :p2 => p2}
+		erb :results, :locals => {:result => result, :p1 => p1, :p2 => p2}
 	elsif result == "subtraction"
-		erb :subtraction, :locals => {:p1 => p1, :p2 => p2}
+		erb :results, :locals => {:result => result, :p1 => p1, :p2 => p2}
 	elsif result == "division"
-		erb :division, :locals => {:p1 => p1, :p2 => p2}
+		erb :results, :locals => {:result => result, :p1 => p1, :p2 => p2}
 	elsif result == "multiplication"
-		erb :multiplication, :locals => {:p1 => p1, :p2 => p2}
+		# erb :results, :locals => {:result => result, :p1 => p1, :p2 => p2}
 	else
 		puts "Sorry, your answer must be addition, subraction, division or multiplication!"
 	end
